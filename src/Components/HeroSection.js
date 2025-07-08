eb0aa2051d1be340dcd4b7edd4438cfaf14eeaf3
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-08 20:55:51 +0200

    updated the structure

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,27 +1,32 @@
 import './HeroSection';
 
 function HeroSection(){
-    return(
-        < div className="Hero-grid">
-        <h1>Simplify HR </h1>
+    return(<>
+        <h1 className="header-message">Simplify HR </h1>
         <h2>Empower Teams</h2>
+        < div className="Hero-grid">
+       
         <div className="Hero-image">
-         <img src="/images/hero-image.png" alt=" " />
+         <img src="/images/hero-image.png" alt=" " className="HeroImg" />
         </div>
-
+          
           <div className="text">
-          <ul>
+            <h1 className="title-platform">All-in-one Platform</h1>
+          <ul className="list">
           <li><img src="/images/check.png" alt ="" className="checkmark"/>Payroll</li>
           <li><img src="/images/check.png " alt=" " className="checkmark"/>Leave</li>
           <li><img src="/images/check.png "  alt="" className="checkmark"/>Contract</li>
           <li><img src="/images/check.png " alt=" " className="checkmark"/>Appointment</li>
+          
           </ul>
+          <p>New here?</p>
           <button>Register</button>
           </div>
           
 
 
         </div>
+        </>
     )
 }
 export default HeroSection;
