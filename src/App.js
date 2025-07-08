c334701e8b1bd9111b44924e37a5132106d6c16a
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-08 19:03:37 +0200

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import Landing from './Pages/Landing';
+import Navbar from './Components/Navbar'; 
 import './App.css';
+import HeroSection from './Components/HeroSection';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    
+    <Navbar />
+     <Landing />
+     <HeroSection />
       
-    </div>
+      
+      
+       
+       
+       
+    </>
   );
 }
 
