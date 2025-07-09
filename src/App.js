9a04067577b1e23537d078182aff2dc4cec1cf26
flash3105 <flash3105findme@gmail.com>
2025-07-09 09:52:43 +0200

    new changes , landing page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import Navbar from './Components/Navbar'; 
 import './App.css';
 import HeroSection from './Components/HeroSection';
+import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/RegisterForm';
 
 function App() {
   return (
     <>
     
     <Navbar />
-     
+     <LoginForm />
+     <RegisterForm />
      <HeroSection />
       
       
