8f0a6d30c0cc51e1081fa0e732f5839c9f8c75c1
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-09 11:56:12 +0200

    Added hero section

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -7,10 +7,32 @@ import './Landing.css';
 function Landing() {
     return (
         <>
+           <h1 className="header-message">Simplify HR </h1> 
+            <h2>Empower Teams</h2>
+
+            < div className="Hero-grid">
+       
+        <div className="Hero-image">
+         <img src="/images/hero-image.png" alt=" " className="HeroImg" />
+        </div>
+
+               <div className="text">
+            <h1 className="title-platform">All-in-one Platform</h1>
+          <ul className="list">
+          <li><i class="fa-regular fa-circle-check"/>Payroll</li>
+          <li><i class="fa-regular fa-circle-check"/>Leave</li>
+          <li><i class="fa-regular fa-circle-check"/>Contract</li>
+          <li><i class="fa-regular fa-circle-check"/>Appointment</li>
+          </ul>
+          <p>New here?</p>
+             <RegisterForm />
+          </div>
+
+
+        </div>
             
             
             
-            <RegisterForm />
         </>
     );
 }
