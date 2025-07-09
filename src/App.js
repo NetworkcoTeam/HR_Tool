f96afbd1847811383de23a64a62cb831a2ba12b0
flash3105 <flash3105findme@gmail.com>
2025-07-09 09:59:29 +0200

    work from here

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 //import Landing from './Pages/Landing';
 import Navbar from './Components/Navbar'; 
 import './App.css';
-import HeroSection from './Components/HeroSection';
-import LoginForm from './Components/LoginForm';
-import RegisterForm from './Components/RegisterForm';
+//import HeroSection from './Components/HeroSection';
+import Landing from './Pages/Landing';
 
 function App() {
   return (
     <>
     
     <Navbar />
-     <LoginForm />
-     <RegisterForm />
-     <HeroSection />
+     
+      <Landing />
+    
+   {/*  <HeroSection /> */} 
       
       
       
---
+++ src/Pages/Landing.js
@@ -0,0 +1,18 @@
+import LoginForm from '../Components/LoginForm';
+import RegisterForm from '../Components/RegisterForm';
+import './Landing.css';
+
+
+
+function Landing() {
+    return (
+        <>
+            
+            
+            <LoginForm />
+            <RegisterForm />
+        </>
+    );
+}
+
+export default Landing;
