c60d5fa381a2dcdc5f4cd419448aba56204b0f0a
flash3105 <flash3105findme@gmail.com>
2025-07-11 09:43:35 +0200

    fixed react

--- HR_Tool.Api/obj/Debug/net8.0/HR_Tool.Api.AssemblyInfo.cs
+++ HR_Tool.Api/obj/Debug/net8.0/HR_Tool.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR_Tool.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48db8f750cd8a8ba5caae2f13dd6fb559f7347fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8caa6843849a867834f44f7ca22fafd2dec69e65")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR_Tool.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR_Tool.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
