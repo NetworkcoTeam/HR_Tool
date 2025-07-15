9d0286b34c2ffc52ff30e7b195e58c94103d6a18
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-15 09:59:57 +0200

    commit api changes

---
+++ src/Pages/Home.js
@@ -0,0 +1,59 @@
+import './Home.css';
+import Sidebar from '../Components/Sidebar';
+
+function home(){
+    return(
+        <>
+        <div className="sideBar">
+            <Sidebar />
+        </div>
+        <div className="dashboard">
+       
+         <div className="first-grid">
+            <div className="Todo-List">
+                <h1> TO DO LIST</h1> 
+                   <div className="Todo-list-container">
+                    <div className="Details"></div>
+                    <div className="time"></div>
+                    </div>
+                    <button>Add to list</button>
+
+            </div>
+
+            <div className="Payslips">
+                <h1> PAYSLIPS</h1> 
+                   <div className="Payslips-container">
+                    <div className="month"></div>
+                    <div className="Payslip-btns"></div>
+                    </div>
+            </div>
+
+
+
+         </div>
+         <div className="second-grid">
+            <div className="Leave">
+                <h1> LEAVE</h1> 
+                   <div className="leave-container">
+                    <div className="Details"></div>
+                    <div className="time"></div>
+                    </div>
+
+             </div>
+
+                 <div className="Appointments">
+                    <h1> APPOINTMENTS</h1> <h1>10 July 2025</h1>
+                   <div className="appointment-container">
+                    <div className="Details"></div>
+                    <div className="time"></div>
+                    </div>
+
+                    </div>
+                    
+         </div>
+
+        </div>
+        </>
+    )
+}
+export default home;
