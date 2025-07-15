c054b03e7da78740af09dbb2c2e64e42c4e2d6ae
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-16 01:46:03 +0200

    Added Leave form

--- src/Pages/LeaveForm.js
+++ src/Pages/LeaveForm.js
@@ -1,38 +1,41 @@
 import './LeaveForm.css';
-//import sidebar from '../Components/sidebar';
+import Sidebar from '../Components/Sidebar';
 
 function Leave(){
 return(
+    <>
 <div className="LeaveForm">
 <div className="sidebar"> <Sidebar/></div>
-    <form>
+    <form className=''>
     <label>Name</label>
-    <input type="text">
+    <input type="text"/>
     <label>Surname</label>
-    <input type="text">
+    <input type="text"/>
     <label>Employee ID</label>
-    <input type="text">
+    <input type="text"/>
     <label>Position</label>
-    <input type="text">
+    <input type="text"/>
     <label>Reason for Leave</label>
-    <input type="radio" value='Martenity' className="Martenity">
-     <input type="radio" value='Paternity' className="Martenity">
-      <input type="radio" value='Sick' className="Martenity">
-       <input type="radio" value='Vacation' className="Martenity">
-        <input type="radio" value='other' className="Martenity">
+    <input type="radio" value='Martenity' className="Martenity"/>
+     <input type="radio" value='Paternity' className="Martenity"/>
+      <input type="radio" value='Sick' className="Martenity"/>
+       <input type="radio" value='Vacation' className="Martenity"/>
+        <input type="radio" value='other' className="Martenity"/>
         
 
     <label>If you chose other specify</label>
-    <input type="text" required>
+    <input type="text" required/>
 
     <label>Leave start</label>
-    <input type="text">
+    <input type="text"/>
     <label>Leave end</label>
-    <input type="text">
+    <input type="text"/>
 
     <button>Apply </button>
     </form>
     </div>
+    
+    </>
 )
 }
 
