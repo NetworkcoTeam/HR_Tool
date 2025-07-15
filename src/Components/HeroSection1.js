4b93bf4499ae2e53846447806e1b8b8f457120cc
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-15 21:02:55 +0200

    package json and packages json updated

---
+++ src/Components/HeroSection1.js
@@ -0,0 +1,26 @@
+
+function HeroSection(){
+    return(
+        < div className="Hero-grid">
+        <h1>Simplify HR </h1>
+        <h2>Empower Teams</h2>
+        <div className="Hero-image">
+         <img src="/images/hero-image.png" alt=" " />
+        </div>
+
+          <div className="text">
+          <ul>
+          <li><img src="/images/check.png" alt ="" className="checkmark"/>Payroll</li>
+          <li><img src="/images/check.png " alt=" " className="checkmark"/>Leave</li>
+          <li><img src="/images/check.png "  alt="" className="checkmark"/>Contract</li>
+          <li><img src="/images/check.png " alt=" " className="checkmark"/>Appointment</li>
+          </ul>
+          <button>Register</button>
+          </div>
+          
+
+
+        </div>
+    )
+}
+export default HeroSection;
