1642f43590e1165c7ae23489b21723ab78b59e2a
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-16 12:41:57 +0200

    Added useState for form data, loop to indicate if the application was successful or not. declared variabbles handleChange and hadleSubmit.

--- src/Pages/LeaveForm.js
+++ src/Pages/LeaveForm.js
@@ -1,31 +1,91 @@
 import './LeaveForm.css';
 import Sidebar from '../Components/Sidebar';
+import React, {useState} from 'react';
 
 function Leave(){
+
+    const[formData, setFormData]= useState({
+        name :'',
+        surname : '',
+        employeeID :'',
+        department : '',
+        position : '',
+        leaveType :'',
+        startDate : '',
+        endDate :'',
+        totalDays:'',
+        doctorletter: '',
+        funeralLetter:''
+
+    })
+
+    const handleChange =(e) =>{
+      setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+    try{
+        const response = await fetch('http://localhost:5143/api/leaveform', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          surname: formData.surname,
+          employeeID: formData.employeeID,
+          department: formData.department,
+          position: formData.position,
+          leaveType : formData.leaveType,
+          startDate : formData.startDate,
+          endDate : formData.endDate,
+          totalDays: formData.totalDays,
+          doctorletter: formData.doctorletter,
+          funeralLetter: formData.funeralLetter
+        }),
+      });
+
+
+         if (response.ok) {
+        console.log('leave application successful!');
+        // Optional: Redirect or show success message
+      } else {
+        console.error('application failed.');
+        // Handle errors — maybe show a message
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+    
+
 return(
     <>
 <div className="LeaveForm">
     
 <div className="sidebar"> <Sidebar/></div>
    
-    <form className="form-area">
+    <form className="form-area" onSubmit={handleSubmit}>
         <div className='form-division'>
 
     <h1>Leave application</h1> 
 
     <div className='left-form'>
     <label>Name</label><br/>
-    <input type="text"/><br/><br/>
+    <input type="text" name='name'/><br/><br/>
     <label>Employee ID</label> <br/>
-    <input type="text"/><br/>
+    <input type="text" name='employeeId'/><br/>
     <label>Position</label><br/>
-    <input type="text"/><br/>
+    <input type="text" name='position'/><br/>
     <label>Leave start</label><br/>
-    <input type="text"/><br/>
+    <input type="date" name='startDay'/><br/>
     <label>Leave end</label><br/>
-    <input type="text"/><br/>
+    <input type="date" name='endDay'/><br/>
     <label>Total days</label>
-    <input type="text"/>
+    <input type="text" name='totalDays'/>
 
     </div>
 
