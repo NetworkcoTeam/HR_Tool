e8429e830e937ab02ef953b4a7ff76f224402116
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-29 09:23:40 +0200

    added prifile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,7 +1,7 @@
 import { DiVim } from 'react-icons/di';
 import Sidebar from '../Components/Sidebar';
 import './Profile.css';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, CalendarOutlined } from '@ant-design/icons';
 import {Avatar ,Space, Divider, Flex} from 'antd';
 
 export default function Profile(){
@@ -13,34 +13,34 @@ return(
         
         
          <div className='Avatar'>
+          <div className='background-avatar'>
         <Space gap='small'>
             
-        <Avatar size={90} icon={<UserOutlined/> } className='avatar'/><br/>
-        <h1>Dimpho Setsile</h1>
+        <Avatar size={190} icon={<UserOutlined/> } className='avatar'/><br/>
+        
         </Space>
+        
+        <Divider/>
+        <div>
+        <h1>Dimpho Setsile</h1>
+        </div>
+        </div>
         </div>
         
         <div className='personal-details'>
         <div className="left" >
-        <label className='Name'> Name:</label><br/>
-         <label className='surname'>Surname:</label><br/>
-         
-         
-         
-           <label className='email'>email address:</label><br/>
-           <label className='dob'>date of birth:</label>
+           <label className='a'>email address:</label><br/><br/>
+           <label className='a'>date of birth</label><br/><br/>
+            <label className='a'>Started working</label><br/>
           </div>
           
           
 
           <div gap='middle' horizontal className='right'>
-           <label className='employeeid'>employee ID :</label><br/><br/>
-           <label className='position'>Position :</label><br/><br/>
-           
-               
-           
-            <label className='department'>Department:</label><br/><br/>
-             <label className='status'>Status Leave:</label><br/><br/>
+           <label className='a'>employee ID </label><br/><br/>
+           <label className='a'>Position </label><br/><br/>
+            <label className='a'>Department</label><br/><br/>
+            
             
              </div>
              </div>
