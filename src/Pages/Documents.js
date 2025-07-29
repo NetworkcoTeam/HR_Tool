71bc8a19381b421fc5a1fd4b27b288e694051f92
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-29 09:59:02 +0200

    added documents page

--- src/Pages/Documents.js
+++ src/Pages/Documents.js
@@ -1,71 +1,16 @@
 import Sidebar from '../Components/Sidebar';
 import './Documents.css';
-import {Table, Divider, Flex} from 'antd';
+import Documents from '../Components/Documents'
 
  
 
-function Documents(){ 
-    const columns = [
-    {
-      title: 'File name',
-      dataIndex: 'file',
-      Key: 'file',
-      
-    },
-    {
-      title: 'view',
-      dataIndex: 'view',
-      key: 'view',
-    },
-    {
-      title: 'Download',
-      dataIndex: 'Download',
-      key: 'download',
-    }]
-    
-    const datasource =[{
-      key:'1',
-      file: 'Contract',
-      view: 'view',
-      Download: 'download',
-    },
-    {
-    key:2,
-      file: 'CV',
-      view: 'view',
-      Download:' download',
-    },
-    {
-    key:3,
-      file: 'Identity document',
-      view: 'view',
-      Download: 'download',
-    },
-
-   {
-    key:'4',
-      file: 'Qualifications',
-      view: 'view',
-      Download: 'download',
-    }]
-              
+function DocumentsPage(){ 
     return(
     <>
 
        <Sidebar />
-       <div className='head'>
-       <h1> Documents</h1> </div>
-        <div className="table">
-           <Table 
-              columns={columns} 
-               dataSource={datasource
-               }
-              
-              
-              
-            />
-            </div>
+       <Documents />
       </>
     )
 }
-export default Documents;
+export default DocumentsPage;
