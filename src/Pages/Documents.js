7ac634585834861d2232e6bf943326253f4e00e0
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-29 09:23:14 +0200

    added document page

--- src/Pages/Documents.js
+++ src/Pages/Documents.js
@@ -1,46 +1,71 @@
 import Sidebar from '../Components/Sidebar';
 import './Documents.css';
+import {Table, Divider, Flex} from 'antd';
 
-function Documents(){
+ 
+
+function Documents(){ 
+    const columns = [
+    {
+      title: 'File name',
+      dataIndex: 'file',
+      Key: 'file',
+      
+    },
+    {
+      title: 'view',
+      dataIndex: 'view',
+      key: 'view',
+    },
+    {
+      title: 'Download',
+      dataIndex: 'Download',
+      key: 'download',
+    }]
+    
+    const datasource =[{
+      key:'1',
+      file: 'Contract',
+      view: 'view',
+      Download: 'download',
+    },
+    {
+    key:2,
+      file: 'CV',
+      view: 'view',
+      Download:' download',
+    },
+    {
+    key:3,
+      file: 'Identity document',
+      view: 'view',
+      Download: 'download',
+    },
+
+   {
+    key:'4',
+      file: 'Qualifications',
+      view: 'view',
+      Download: 'download',
+    }]
+              
     return(
-     <div className='Documents-container'>
-        <Sidebar />
-        <h1>Documents</h1>
-        <div className='Document-container'>
-            <div className='file-container'>
-            <label>Contract </label>
+    <>
 
-            <div className='btns'>
-            <button>view</button>
-            <button>Download</button>
+       <Sidebar />
+       <div className='head'>
+       <h1> Documents</h1> </div>
+        <div className="table">
+           <Table 
+              columns={columns} 
+               dataSource={datasource
+               }
+              
+              
+              
+            />
             </div>
-             </div>
-
-            <div className='file-container'>
-            <label> CV</label>
-              <div className='btns'>
-            <button>view</button>
-            <button>Download</button>
-             </div>
-            </div>
-
-             <div className='file-container'>
-            <label>Identity document </label>
-              <div className='btns'></div>
-             <button>view</button>
-            <button>Download</button>
-             </div>
-            </div>
-
-            <div className='file-container'>
-            <label> Qualifications </label>
-              <div className='btns'>
-             <button>view</button>
-            <button>Download</button>
-            </div>
-        </div>
-
-      </div>
+      </>
     )
 }
 export default Documents;
